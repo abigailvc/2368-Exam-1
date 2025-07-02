d286d7544b47f0fdd46401fe6d36c4549c13605e
abigailvc <avleipar@cougarnet.uh.edu>
2025-07-02 09:24:04 -0500

    Added database connection utilities

---
+++ creds.py
@@ -0,0 +1,9 @@
+# Database credentials file for Exam 1 - Troop API
+# Adapted from Homework 2 (Zoo API)
+
+class Creds:
+    def __init__(self):
+        self.hostname = "cis2368summer.cgq5dpblmutk.us-east-1.rds.amazonaws.com"
+        self.uname = "admin"
+        self.passwd = "Summer25DBpass!"
+        self.dbname = "cis2368summerdb"
